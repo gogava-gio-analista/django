d5da024d96c04fbe582e6b40fd2a204021dcb164
gogava-gio-analista <giorggogava@gmail.com>
2026-02-18 20:23:21 +0100

    ass

--- webproject/api01/urls.py
+++ webproject/api01/urls.py
@@ -3,7 +3,12 @@ from api01 import views
 
 urlpatterns=[
     path('prueba/', views.prueba, name='prueba'),
-    path('index/', views.index, name='index')
+    path('index/', views.index, name='index'),
+    path('peliculas/', views.peliculas, name='peliculas'),
+    path('futbol/', views.futbol, name='futbol'),
+    path('nombres/', views.nombres, name='nombres'),
+    path('mascotas/', views.mascotas, name='mascotas'),
+    path('colores/', views.colores, name='colores')
 ]
 
 # Parámetros método path:
--- webproject/api01/views.py
+++ webproject/api01/views.py
@@ -7,3 +7,82 @@ def index(request):
 
 def prueba(request):
     return render(request, 'paginas/prueba.html')
+
+def peliculas(request):
+    return  render(request, "paginas/peliculas.html")
+
+def futbol(request):
+    informacion = {
+        'equipo':'Bilbao'
+    }
+    return render(request, 'paginas/futbol.html', informacion)
+
+def nombres(request):
+    lista = ['giorgi', 'david', 'alejandra', 'selena', 'ruby']
+    listapersona = [
+        {
+            'nombre':'novaria',
+            'edad':23
+        },
+        {
+            'nombre':'selena',
+            'edad':24
+        },
+        {
+            'nombre':'martis',
+            'edad':26
+        },
+        {
+            'nombre':'ginever',
+            'edad':21
+        }
+    ]
+    informacion = {
+        'listanombres': lista,
+        'listapersona':listapersona
+    }
+    
+    return render(request, 'paginas/nombres.html', informacion)
+
+def mascotas(request):
+    listamascotas = [
+        {
+            'nombre':'jeka',
+            'raza':'perro',
+            'edad':8
+        },
+        {
+            'nombre':'bob',
+            'raza':'gato',
+            'edad':5
+        },
+        {
+            'nombre':'susi',
+            'raza':'vaca',
+            'edad':5
+        },
+        {
+            'nombre':'random',
+            'raza':'serpiente',
+            'edad':9
+        },
+        {
+            'nombre':'jeka',
+            'raza':'perro',
+            'edad':8
+        }
+    ]
+    info = {
+        'lista':listamascotas
+    }
+    return render(request, 'paginas/mascotas.html', info)
+
+def colores(request):
+    if ('micolor' in request.GET):
+        colorrecibido = request.GET['micolor']
+    else:
+        colorrecibido = "Nada"
+    context = {
+        'color':colorrecibido
+    }
+    return render(request, 'paginas/colores.html', context)
